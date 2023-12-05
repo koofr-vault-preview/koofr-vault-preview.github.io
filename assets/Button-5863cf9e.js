650ff4f4850bb515d7aba7e4b0cbaa00c98712dd
bancek <367440+bancek@users.noreply.github.com>
2023-12-05 19:03:09 +0000

    Update assets

---
+++ assets/Button-5863cf9e.js
@@ -0,0 +1,54 @@
+import{q as j,_ as N,w as U,r as p,T as q,s as V,t as W,v as X,x as K,y as Y,b as T,j as _,u as F,d as O}from"./DynamicThemeProvider-852093d9.js";import{L as G}from"./index-1ee9421e.js";var Z=/^((children|dangerouslySetInnerHTML|key|ref|autoFocus|defaultValue|defaultChecked|innerHTML|suppressContentEditableWarning|suppressHydrationWarning|valueLink|abbr|accept|acceptCharset|accessKey|action|allow|allowUserMedia|allowPaymentRequest|allowFullScreen|allowTransparency|alt|async|autoComplete|autoPlay|capture|cellPadding|cellSpacing|challenge|charSet|checked|cite|classID|className|cols|colSpan|content|contentEditable|contextMenu|controls|controlsList|coords|crossOrigin|data|dateTime|decoding|default|defer|dir|disabled|disablePictureInPicture|download|draggable|encType|enterKeyHint|form|formAction|formEncType|formMethod|formNoValidate|formTarget|frameBorder|headers|height|hidden|high|href|hrefLang|htmlFor|httpEquiv|id|inputMode|integrity|is|keyParams|keyType|kind|label|lang|list|loading|loop|low|marginHeight|marginWidth|max|maxLength|media|mediaGroup|method|min|minLength|multiple|muted|name|nonce|noValidate|open|optimum|pattern|placeholder|playsInline|poster|preload|profile|radioGroup|readOnly|referrerPolicy|rel|required|reversed|role|rows|rowSpan|sandbox|scope|scoped|scrolling|seamless|selected|shape|size|sizes|slot|span|spellCheck|src|srcDoc|srcLang|srcSet|start|step|style|summary|tabIndex|target|title|translate|type|useMap|value|width|wmode|wrap|about|datatype|inlist|prefix|property|resource|typeof|vocab|autoCapitalize|autoCorrect|autoSave|color|incremental|fallback|inert|itemProp|itemScope|itemType|itemID|itemRef|on|option|results|security|unselectable|accentHeight|accumulate|additive|alignmentBaseline|allowReorder|alphabetic|amplitude|arabicForm|ascent|attributeName|attributeType|autoReverse|azimuth|baseFrequency|baselineShift|baseProfile|bbox|begin|bias|by|calcMode|capHeight|clip|clipPathUnits|clipPath|clipRule|colorInterpolation|colorInterpolationFilters|colorProfile|colorRendering|contentScriptType|contentStyleType|cursor|cx|cy|d|decelerate|descent|diffuseConstant|direction|display|divisor|dominantBaseline|dur|dx|dy|edgeMode|elevation|enableBackground|end|exponent|externalResourcesRequired|fill|fillOpacity|fillRule|filter|filterRes|filterUnits|floodColor|floodOpacity|focusable|fontFamily|fontSize|fontSizeAdjust|fontStretch|fontStyle|fontVariant|fontWeight|format|from|fr|fx|fy|g1|g2|glyphName|glyphOrientationHorizontal|glyphOrientationVertical|glyphRef|gradientTransform|gradientUnits|hanging|horizAdvX|horizOriginX|ideographic|imageRendering|in|in2|intercept|k|k1|k2|k3|k4|kernelMatrix|kernelUnitLength|kerning|keyPoints|keySplines|keyTimes|lengthAdjust|letterSpacing|lightingColor|limitingConeAngle|local|markerEnd|markerMid|markerStart|markerHeight|markerUnits|markerWidth|mask|maskContentUnits|maskUnits|mathematical|mode|numOctaves|offset|opacity|operator|order|orient|orientation|origin|overflow|overlinePosition|overlineThickness|panose1|paintOrder|pathLength|patternContentUnits|patternTransform|patternUnits|pointerEvents|points|pointsAtX|pointsAtY|pointsAtZ|preserveAlpha|preserveAspectRatio|primitiveUnits|r|radius|refX|refY|renderingIntent|repeatCount|repeatDur|requiredExtensions|requiredFeatures|restart|result|rotate|rx|ry|scale|seed|shapeRendering|slope|spacing|specularConstant|specularExponent|speed|spreadMethod|startOffset|stdDeviation|stemh|stemv|stitchTiles|stopColor|stopOpacity|strikethroughPosition|strikethroughThickness|string|stroke|strokeDasharray|strokeDashoffset|strokeLinecap|strokeLinejoin|strokeMiterlimit|strokeOpacity|strokeWidth|surfaceScale|systemLanguage|tableValues|targetX|targetY|textAnchor|textDecoration|textRendering|textLength|to|transform|u1|u2|underlinePosition|underlineThickness|unicode|unicodeBidi|unicodeRange|unitsPerEm|vAlphabetic|vHanging|vIdeographic|vMathematical|values|vectorEffect|version|vertAdvY|vertOriginX|vertOriginY|viewBox|viewTarget|visibility|widths|wordSpacing|writingMode|x|xHeight|x1|x2|xChannelSelector|xlinkActuate|xlinkArcrole|xlinkHref|xlinkRole|xlinkShow|xlinkTitle|xlinkType|xmlBase|xmlns|xmlnsXlink|xmlLang|xmlSpace|y|y1|y2|yChannelSelector|z|zoomAndPan|for|class|autofocus)|(([Dd][Aa][Tt][Aa]|[Aa][Rr][Ii][Aa]|x)-.*))$/,J=j(function(t){return Z.test(t)||t.charCodeAt(0)===111&&t.charCodeAt(1)===110&&t.charCodeAt(2)<91}),Q=J,ee=function(e){return e!=="theme"},E=function(e){return typeof e=="string"&&e.charCodeAt(0)>96?Q:ee},H=function(e,r,n){var o;if(r){var a=r.shouldForwardProp;o=e.__emotion_forwardProp&&a?function(s){return e.__emotion_forwardProp(s)&&a(s)}:a}return typeof o!="function"&&n&&(o=e.__emotion_forwardProp),o},te=function(e){var r=e.cache,n=e.serialized,o=e.isStringTag;return X(r,n,o),K(function(){return Y(r,n,o)}),null},re=function t(e,r){var n=e.__emotion_real===e,o=n&&e.__emotion_base||e,a,s;r!==void 0&&(a=r.label,s=r.target);var g=H(e,r,n),v=g||E(o),m=!v("as");return function(){var c=arguments,d=n&&e.__emotion_styles!==void 0?e.__emotion_styles.slice(0):[];if(a!==void 0&&d.push("label:"+a+";"),c[0]==null||c[0].raw===void 0)d.push.apply(d,c);else{d.push(c[0][0]);for(var u=c.length,h=1;h<u;h++)d.push(c[h],c[0][h])}var l=U(function(i,f,D){var w=m&&i.as||o,y="",I=[],b=i;if(i.theme==null){b={};for(var L in i)b[L]=i[L];b.theme=p.useContext(q)}typeof i.className=="string"?y=V(f.registered,I,i.className):i.className!=null&&(y=i.className+" ");var B=W(d.concat(I),f.registered,b);y+=f.key+"-"+B.name,s!==void 0&&(y+=" "+s);var M=m&&g===void 0?E(w):v,x={};for(var k in i)m&&k==="as"||M(k)&&(x[k]=i[k]);return x.className=y,x.ref=D,p.createElement(p.Fragment,null,p.createElement(te,{cache:f,serialized:B,isStringTag:typeof w=="string"}),p.createElement(w,x))});return l.displayName=a!==void 0?a:"Styled("+(typeof o=="string"?o:o.displayName||o.name||"Component")+")",l.defaultProps=e.defaultProps,l.__emotion_real=l,l.__emotion_base=o,l.__emotion_styles=d,l.__emotion_forwardProp=g,Object.defineProperty(l,"toString",{value:function(){return"."+s}}),l.withComponent=function(i,f){return t(i,N({},r,f,{shouldForwardProp:H(l,f,!0)})).apply(void 0,d)},l}},oe=["a","abbr","address","area","article","aside","audio","b","base","bdi","bdo","big","blockquote","body","br","button","canvas","caption","cite","code","col","colgroup","data","datalist","dd","del","details","dfn","dialog","div","dl","dt","em","embed","fieldset","figcaption","figure","footer","form","h1","h2","h3","h4","h5","h6","head","header","hgroup","hr","html","i","iframe","img","input","ins","kbd","keygen","label","legend","li","link","main","map","mark","marquee","menu","menuitem","meta","meter","nav","noscript","object","ol","optgroup","option","output","p","param","picture","pre","progress","q","rp","rt","ruby","s","samp","script","section","select","small","source","span","strong","style","sub","summary","sup","table","tbody","td","textarea","tfoot","th","thead","time","title","tr","track","u","ul","var","video","wbr","circle","clipPath","defs","ellipse","foreignObject","g","image","line","linearGradient","mask","path","pattern","polygon","polyline","radialGradient","rect","stop","svg","text","tspan"],C=re.bind();oe.forEach(function(t){C[t]=C(t)});const A="&, &:focus, &:active, &:hover",ne=`background: none;
+border: none;
+appearance: none;
+cursor: pointer;
+padding: 0;
+
+${A} {
+  outline: 0;
+  text-decoration: none;
+}`,ae="background-color 0.2s ease-in-out, border-color 0.2s ease-in-out, color 0.2s ease-in-out",S=(t,e,r,n,o,a)=>T`
+  background-color: ${t};
+  border-color: ${e};
+
+  ${A} {
+    color: ${r};
+  }
+
+  &:hover,
+  &:focus {
+    background-color: ${n};
+    border-color: ${o};
+    color: ${a};
+  }
+`,P=(t,e)=>T`
+  background: none;
+  border: none;
+
+  ${A} {
+    color: ${t};
+  }
+
+  &:hover {
+    color: ${e};
+  }
+`,ie=["as","disabled"];function se(t,e){if(t==null)return{};var r={},n=Object.keys(t),o,a;for(a=0;a<n.length;a++)o=n[a],!(e.indexOf(o)>=0)&&(r[o]=t[o]);return r}function le(t){return!t||t.trim()==="#"}function ce({tagName:t,disabled:e,href:r,target:n,rel:o,role:a,onClick:s,tabIndex:g=0,type:v}){t||(r!=null||n!=null||o!=null?t="a":t="button");const m={tagName:t};if(t==="button")return[{type:v||"button",disabled:e},m];const c=u=>{if((e||t==="a"&&le(r))&&u.preventDefault(),e){u.stopPropagation();return}s==null||s(u)},d=u=>{u.key===" "&&(u.preventDefault(),c(u))};return t==="a"&&(r||(r="#"),e&&(r=void 0)),[{role:a??"button",disabled:void 0,tabIndex:e?void 0:g,href:r,target:t==="a"?n:void 0,"aria-disabled":e||void 0,rel:t==="a"?o:void 0,onClick:c,onKeyDown:d},m]}const z=p.forwardRef((t,e)=>{let{as:r,disabled:n}=t,o=se(t,ie);const[a,{tagName:s}]=ce(Object.assign({tagName:r,disabled:n},o));return _.jsx(s,Object.assign({},o,a,{ref:e}))});z.displayName="Button";const de=z,R=C(de)`
+  ${ne}
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 3px;
+  text-align: center;
+  display: inline-block;
+  font-weight: 600;
+  font-size: 13px;
+  padding: 8px 15px 8px;
+  line-height: ${t=>t.theme.lineHeightBase};
+  flex-shrink: 0;
+
+  &:hover,
+  &:focus {
+    transition: ${ae};
+  }
+`,ue=R.withComponent(G),me=R.withComponent("a");function $(t,e){switch(t){case"primary":return S(e.colors.primary,e.colors.primary,e.colors.textInverse,e.colors.primaryHover,e.colors.primaryHover,e.colors.textInverse);case"primary-inline":return P(e.colors.primary,e.colors.primaryHover);case"destructive":return S(e.colors.destructive,e.colors.destructive,e.colors.textInverse,e.colors.destructiveHover,e.colors.destructiveHover,e.colors.textInverse);case"destructive-inline":return P(e.colors.destructive,e.colors.destructiveHover);case"disabled":return O(S(e.colors.disabled,e.colors.disabled,e.colors.textInverse,e.colors.disabled,e.colors.disabled,e.colors.textInverse),T`
+          cursor: default;
+        `);case"disabled-inline":return P(e.colors.disabled,e.colors.disabled);default:return S("transparent",e.colors.borderDarker,e.colors.text,"transparent",e.colors.text,e.colors.text)}}const ge=p.memo(({variant:t,className:e,...r})=>{const n=F();return _.jsx(R,{className:O($(t,n),e),...r})}),ye=p.memo(({variant:t,className:e,...r})=>{const n=F();return _.jsx(ue,{className:O($(t,n),e),...r})});export{ge as B,ye as L,S as a,ne as b,me as c,A as d,de as e,ae as f,R as g,C as n};
---
+++ assets/GitRevision-aae7f3a0.js
@@ -0,0 +1,5 @@
+import{r as e,j as t,b as o}from"./DynamicThemeProvider-852093d9.js";import{d as i}from"./Button-5863cf9e.js";const c=e.memo(({className:s})=>null),m=e.memo(({className:s})=>{const r="cc72289";return t.jsxs("div",{className:s,children:["Git revision:"," ",t.jsx("a",{href:`https://github.com/koofr/vault/commit/${r}`,target:"_blank",rel:"noreferrer",className:o`
+          ${i} {
+            color: inherit;
+          }
+        `,children:r})]})});export{c as G,m as a};
