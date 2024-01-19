8a478c30152d57cf0d5312c53f2067313f96a551
bancek <367440+bancek@users.noreply.github.com>
2024-01-19 18:27:43 +0000

    Update assets

--- assets/GitRevision-914d771b.js
+++
@@ -1,5 +0,0 @@
-import{r as e,j as t,b as o}from"./DynamicThemeProvider-71c37830.js";import{d as i}from"./Button-d7c59dae.js";const m=e.memo(({className:s})=>null),l=e.memo(({className:s})=>{const r="7083004";return t.jsxs("div",{className:s,children:["Git revision:"," ",t.jsx("a",{href:`https://github.com/koofr/vault/commit/${r}`,target:"_blank",rel:"noreferrer",className:o`
-          ${i} {
-            color: inherit;
-          }
-        `,children:r})]})});export{m as G,l as a};
