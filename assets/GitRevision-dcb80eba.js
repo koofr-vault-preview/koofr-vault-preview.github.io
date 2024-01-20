11c242bc60754425d248e6f43c2c92ca68575f3d
bancek <367440+bancek@users.noreply.github.com>
2024-01-20 12:11:08 +0000

    Update assets

--- assets/GitRevision-dcb80eba.js
+++
@@ -1,5 +0,0 @@
-import{r as e,j as t,b as o}from"./DynamicThemeProvider-71c37830.js";import{d as a}from"./Button-d7c59dae.js";const m=e.memo(({className:s})=>null),l=e.memo(({className:s})=>{const r="970a1d1";return t.jsxs("div",{className:s,children:["Git revision:"," ",t.jsx("a",{href:`https://github.com/koofr/vault/commit/${r}`,target:"_blank",rel:"noreferrer",className:o`
-          ${a} {
-            color: inherit;
-          }
-        `,children:r})]})});export{m as G,l as a};
