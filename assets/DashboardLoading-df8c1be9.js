29c6396138b6d3f60733c91c6626befd2efc682c
bancek <367440+bancek@users.noreply.github.com>
2024-05-24 03:15:54 +0000

    Update assets

--- assets/DashboardLoading-df8c1be9.js
+++
@@ -1,5 +0,0 @@
-import{r,j as s,b as o,L as e}from"./DynamicThemeProvider-71c37830.js";import{u as i}from"./useDocumentTitle-2cb3b118.js";import{D as m}from"./DashboardNavbar-0e4191de.js";const c=r.memo(({navbarHeader:a})=>(i("Loading"),s.jsxs(s.Fragment,{children:[s.jsx(m,{header:a??""}),s.jsx("main",{className:o`
-          display: flex;
-          flex-direction: column;
-          flex-grow: 1;
-        `,children:s.jsx(e,{})})]})));export{c as D};
---
+++ assets/GitRevision-4b6b5d86.js
@@ -0,0 +1,5 @@
+import{r as t,j as r,b as o}from"./DynamicThemeProvider-71c37830.js";import{d as i}from"./Button-d7c59dae.js";const m=t.memo(({className:s})=>null),c=t.memo(({className:s})=>{const e="f15e46c";return r.jsxs("div",{className:s,children:["Git revision:"," ",r.jsx("a",{href:`https://github.com/koofr/vault/commit/${e}`,target:"_blank",rel:"noreferrer",className:o`
+          ${i} {
+            color: inherit;
+          }
+        `,children:e})]})});export{m as G,c as a};
