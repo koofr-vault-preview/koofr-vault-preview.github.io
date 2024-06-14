7405bcfa4c1a95321ea4afb61de064a398ae4ed1
bancek <367440+bancek@users.noreply.github.com>
2024-06-14 06:33:45 +0000

    Update assets

---
+++ assets/GitRevision-2aaf540e.js
@@ -0,0 +1,9 @@
+import{r as t,j as r,b as o}from"./DynamicThemeProvider-71c37830.js";import{d as i}from"./Button-d7c59dae.js";const l=t.memo(({className:s})=>{const e="v0.1.14";return r.jsxs("div",{className:s,children:["Version:"," ",r.jsx("a",{href:`https://github.com/koofr/vault/releases/tag/${e}`,target:"_blank",rel:"noreferrer",className:o`
+          ${i} {
+            color: inherit;
+          }
+        `,children:e})]})}),c=t.memo(({className:s})=>{const e="c0028ab";return r.jsxs("div",{className:s,children:["Git revision:"," ",r.jsx("a",{href:`https://github.com/koofr/vault/commit/${e}`,target:"_blank",rel:"noreferrer",className:o`
+          ${i} {
+            color: inherit;
+          }
+        `,children:e})]})});export{l as G,c as a};
