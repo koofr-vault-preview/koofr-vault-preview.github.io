ef9a1f532c0616ed980f8f6a83f3643e3cf4a4ee
bancek <367440+bancek@users.noreply.github.com>
2024-06-20 23:11:24 +0000

    Update assets

---
+++ assets/DashboardLoading-Dj1H3d-l.js
@@ -0,0 +1,5 @@
+import{r as s,j as r}from"./DynamicThemeProvider-CxehgrR0.js";import{c as a}from"./emotion-css.esm-CiHGz3gB.js";import{u as e}from"./useDocumentTitle-D3L4F_h2.js";import{i}from"./mainWebAuthenticated-DyVN5t5q.js";import{D as m}from"./DashboardNavbar-DmiQf-Bq.js";const l=s.memo(({navbarHeader:o})=>(e("Loading"),r.jsxs(r.Fragment,{children:[r.jsx(m,{header:o??""}),r.jsx("main",{className:a`
+          display: flex;
+          flex-direction: column;
+          flex-grow: 1;
+        `,children:r.jsx(i,{})})]})));export{l as D};
--- assets/GitRevision-BFgkPyk5.js
+++
@@ -1,5 +0,0 @@
-import{r as e,j as t}from"./DynamicThemeProvider-CxehgrR0.js";import{c as o}from"./emotion-css.esm-CiHGz3gB.js";import{d as a}from"./Button-Dt6jELX4.js";const c=e.memo(({className:r})=>null),l=e.memo(({className:r})=>{const s="7ae048a";return t.jsxs("div",{className:r,children:["Git revision:"," ",t.jsx("a",{href:`https://github.com/koofr/vault/commit/${s}`,target:"_blank",rel:"noreferrer",className:o`
-          ${a} {
-            color: inherit;
-          }
-        `,children:s})]})});export{c as G,l as a};
