69033a36fb43c8e70994aecd155c17afdfa58bb1
jkmcnk <4378371+jkmcnk@users.noreply.github.com>
2025-05-05 14:49:45 +0000

    Update assets

--- assets/DashboardLoading-CuCxS8_5.js
+++
@@ -1,5 +0,0 @@
-import{r as s,j as r}from"./DynamicThemeProvider-CxehgrR0.js";import{c as a}from"./emotion-css.esm-CiHGz3gB.js";import{u as e}from"./useDocumentTitle-D3L4F_h2.js";import{i}from"./mainWebAuthenticated-CsofEDA6.js";import{D as m}from"./DashboardNavbar-D5FkUPs1.js";const l=s.memo(({navbarHeader:o})=>(e("Loading"),r.jsxs(r.Fragment,{children:[r.jsx(m,{header:o??""}),r.jsx("main",{className:a`
-          display: flex;
-          flex-direction: column;
-          flex-grow: 1;
-        `,children:r.jsx(i,{})})]})));export{l as D};
