14da63fa435b9170c3ad248ae49dc2b046e569cb
jkmcnk <4378371+jkmcnk@users.noreply.github.com>
2025-05-05 15:13:51 +0000

    Update assets

---
+++ assets/GitRevision-ClhB6tiZ.js
@@ -0,0 +1,5 @@
+import{r as e,j as t}from"./DynamicThemeProvider-CxehgrR0.js";import{c as o}from"./emotion-css.esm-CiHGz3gB.js";import{d as i}from"./Button-Dt6jELX4.js";const n=e.memo(({className:r})=>null),l=e.memo(({className:r})=>{const s="1ac11cf";return t.jsxs("div",{className:r,children:["Git revision:"," ",t.jsx("a",{href:`https://github.com/koofr/vault/commit/${s}`,target:"_blank",rel:"noreferrer",className:o`
+          ${i} {
+            color: inherit;
+          }
+        `,children:s})]})});export{n as G,l as a};
