5de9f4fdc9d63452af0c9785126741fbed580857
bancek <367440+bancek@users.noreply.github.com>
2026-01-01 14:31:49 +0000

    Update assets

--- assets/GitRevision-CbTPLnkN.js
+++
@@ -1,5 +0,0 @@
-import{r,j as t}from"./DynamicThemeProvider-MqskYVZg.js";import{c as i}from"./emotion-css.esm-D9bkNW-b.js";import{d as a}from"./Button-D3442RQe.js";const o=r.memo(({className:e})=>null);o.displayName="GitRelease";const m=r.memo(({className:e})=>{const s="e97713a";return t.jsxs("div",{className:e,children:["Git revision:"," ",t.jsx("a",{href:`https://github.com/koofr/vault/commit/${s}`,target:"_blank",rel:"noreferrer",className:i`
-          ${a} {
-            color: inherit;
-          }
-        `,children:s})]})});m.displayName="GitRevision";export{o as G,m as a};
