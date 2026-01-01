2abe0fb0a25a4428b7f6cf7df8f133f295d560fc
bancek <367440+bancek@users.noreply.github.com>
2026-01-01 13:13:16 +0000

    Update assets

---
+++ assets/DashboardLoading-r2VmRkgu.js
@@ -0,0 +1,5 @@
+import{r as o,j as a}from"./DynamicThemeProvider-MqskYVZg.js";import{c as s}from"./emotion-css.esm-D9bkNW-b.js";import{u as e}from"./useDocumentTitle-D3L4F_h2.js";import{i}from"./mainWebAuthenticated-vtYVy613.js";import{D as m}from"./DashboardNavbar-DtM_YPZV.js";const t=o.memo(({navbarHeader:r})=>(e("Loading"),a.jsxs(a.Fragment,{children:[a.jsx(m,{header:r??""}),a.jsx("main",{className:s`
+          display: flex;
+          flex-direction: column;
+          flex-grow: 1;
+        `,children:a.jsx(i,{})})]})));t.displayName="DashboardLoading";export{t as D};
