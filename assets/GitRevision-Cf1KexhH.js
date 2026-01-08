fc4e0640222222ebdacf656384386a93206b370c
overlordtm <220832+overlordtm@users.noreply.github.com>
2026-01-08 13:44:44 +0000

    Update assets

--- assets/GitRevision-Cf1KexhH.js
+++
@@ -1,5 +0,0 @@
-import{r,j as t}from"./DynamicThemeProvider-MqskYVZg.js";import{c as i}from"./emotion-css.esm-D9bkNW-b.js";import{d as o}from"./Button-D3442RQe.js";const a=r.memo(({className:e})=>null);a.displayName="GitRelease";const m=r.memo(({className:e})=>{const s="44bf826";return t.jsxs("div",{className:e,children:["Git revision:"," ",t.jsx("a",{href:`https://github.com/koofr/vault/commit/${s}`,target:"_blank",rel:"noreferrer",className:i`
-          ${o} {
-            color: inherit;
-          }
-        `,children:s})]})});m.displayName="GitRevision";export{a as G,m as a};
