50ecec45987fcf92d5d7c3ee8a3bf6594b16e208
bancek <367440+bancek@users.noreply.github.com>
2026-01-15 21:50:38 +0000

    Update assets

---
+++ assets/PasswordInput-NxJ31T9d.js
@@ -0,0 +1,57 @@
+import{r as e,j as s}from"./DynamicThemeProvider-MqskYVZg.js";import{a,c as l}from"./emotion-css.esm-D9bkNW-b.js";import{b as v}from"./Button-D3442RQe.js";import{T as x}from"./mainWebAuthenticated-zAhhv6Xh.js";function E(){const t=e.useRef(null);return e.useEffect(()=>{t.current?.focus()},[t]),t}const b=t=>e.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:18,height:18,...t},e.createElement("g",{fill:"none",fillRule:"evenodd"},e.createElement("path",{d:"M0 0h18v18H0z"}),e.createElement("path",{fill:"#263238",d:"M9 3q4.971 0 9 6-4.029 6-9 6c-4.971 0-6.314-2-9-6q4.029-6 9-6m0 1.46c-2.506 0-4.863 1.415-7.098 4.383L1.785 9l.117.157c2.158 2.865 4.43 4.284 6.84 4.378L9 13.54c2.506 0 4.863-1.415 7.098-4.383L16.214 9l-.116-.157c-2.158-2.865-4.43-4.284-6.84-4.378z"}),e.createElement("path",{fill:"#263238",d:"M9 5.5a3.5 3.5 0 1 1 0 7 3.5 3.5 0 0 1 0-7m0 1.46a2.04 2.04 0 1 0 0 4.08 2.04 2.04 0 0 0 0-4.08"}),e.createElement("path",{fill:"#263238",d:"M14.213 15.247a.724.724 0 0 0 1.02.01l.024-.024a.713.713 0 0 0-.01-1.02L3.55 2.515a.724.724 0 0 0-1.02-.01l-.024.024a.713.713 0 0 0 .01 1.02z"}))),y=t=>e.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:18,height:18,...t},e.createElement("g",{fill:"none",fillRule:"evenodd"},e.createElement("path",{fill:"#676f73",fillRule:"nonzero",d:"M9 3q4.971 0 9 6-4.029 6-9 6c-4.971 0-6.314-2-9-6q4.029-6 9-6m0 1.46c-2.506 0-4.863 1.415-7.098 4.383L1.785 9l.117.157c2.158 2.865 4.43 4.284 6.84 4.378L9 13.54c2.506 0 4.863-1.415 7.098-4.383L16.214 9l-.116-.157c-2.158-2.865-4.43-4.284-6.84-4.378z"}),e.createElement("path",{fill:"#676f73",fillRule:"nonzero",d:"M9 5.5a3.5 3.5 0 1 1 0 7 3.5 3.5 0 0 1 0-7m0 1.46a2.04 2.04 0 1 0 0 4.08 2.04 2.04 0 0 0 0-4.08"}),e.createElement("path",{d:"M0 0h18v18H0z"}),e.createElement("path",{fill:"#676f73",fillRule:"nonzero",d:"M14.213 15.247a.724.724 0 0 0 1.02.01l.024-.024a.713.713 0 0 0-.01-1.02L3.55 2.515a.724.724 0 0 0-1.02-.01l-.024.024a.713.713 0 0 0 .01 1.02z"}))),z=t=>e.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:18,height:18,...t},e.createElement("g",{fill:"none",fillRule:"evenodd"},e.createElement("path",{fill:"#263238",fillRule:"nonzero",d:"M9 3q4.971 0 9 6-4.029 6-9 6c-4.971 0-6.314-2-9-6q4.029-6 9-6m0 1.55c-2.475 0-4.809 1.401-7.028 4.35l-.075.1.075.1c2.143 2.847 4.392 4.252 6.772 4.345L9 13.45c2.475 0 4.809-1.401 7.028-4.35l.074-.1-.074-.1c-2.143-2.847-4.392-4.252-6.772-4.345z"}),e.createElement("path",{fill:"#263238",fillRule:"nonzero",d:"M9 5.5a3.5 3.5 0 1 1 0 7 3.5 3.5 0 0 1 0-7m0 1.55a1.95 1.95 0 1 0 0 3.9 1.95 1.95 0 0 0 0-3.9"}),e.createElement("path",{d:"M0 0h18v18H0z"}))),R=t=>e.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:18,height:18,...t},e.createElement("g",{fill:"none",fillRule:"evenodd"},e.createElement("path",{fill:"#676f73",fillRule:"nonzero",d:"M9 3q4.971 0 9 6-4.029 6-9 6c-4.971 0-6.314-2-9-6q4.029-6 9-6m0 1.55c-2.475 0-4.809 1.401-7.028 4.35l-.075.1.075.1c2.143 2.847 4.392 4.252 6.772 4.345L9 13.45c2.475 0 4.809-1.401 7.028-4.35l.074-.1-.074-.1c-2.143-2.847-4.392-4.252-6.772-4.345z"}),e.createElement("path",{fill:"#676f73",fillRule:"nonzero",d:"M9 5.5a3.5 3.5 0 1 1 0 7 3.5 3.5 0 0 1 0-7m0 1.55a1.95 1.95 0 1 0 0 3.9 1.95 1.95 0 0 0 0-3.9"}),e.createElement("path",{d:"M0 0h18v18H0z"}))),r=e.memo(({value:t,onClick:o})=>s.jsx("button",{type:"button",className:l`
+        ${v}
+        cursor: pointer;
+        width: 24px;
+        height: 24px;
+        position: absolute;
+        right: 10px;
+
+        &:focus {
+          outline: none;
+        }
+      `,tabIndex:-1,onClick:o,"aria-label":t?"Hide password":"Show password",children:s.jsxs("div",{className:l`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        `,children:[s.jsx(R,{className:a(t?l`
+                  display: block;
+
+                  button:hover > div > & {
+                    display: none;
+                  }
+                `:l`
+                  display: none;
+                `),role:"img"}),s.jsx(z,{className:a(t?l`
+                  display: none;
+
+                  button:hover > div > & {
+                    display: block;
+                  }
+                `:l`
+                  display: none;
+                `),role:"img"}),s.jsx(y,{className:a(t?l`
+                  display: none;
+                `:l`
+                  display: block;
+
+                  button:hover > div > & {
+                    display: none;
+                  }
+                `),role:"img"}),s.jsx(b,{className:a(t?l`
+                  display: none;
+                `:l`
+                  display: none;
+
+                  button:hover > div > & {
+                    display: block;
+                  }
+                `),role:"img"})]})}));r.displayName="ShowPassword";const c=e.memo(({value:t,placeholder:o,onChange:d,className:m,inputRef:p,inputClassName:h,inputId:f,inputAriaLabel:u})=>{const[i,w]=e.useState(!1),g=e.useCallback(()=>w(n=>!n),[]);return s.jsxs("div",{className:a(l`
+            display: flex;
+            flex-direction: row;
+            position: relative;
+            align-items: center;
+          `,m),children:[s.jsx(x,{type:i?"text":"password",id:f,name:"password",value:t,placeholder:o,onChange:n=>d(n.currentTarget.value),ref:p,className:a(l`
+              font-size: 16px;
+              width: 250px;
+              padding-right: 38px;
+            `,h),"aria-label":u}),s.jsx(r,{value:i,onClick:g})]})});c.displayName="PasswordInput";const M=e.memo(t=>{const o=E();return s.jsx(c,{...t,inputRef:o})});M.displayName="AutoFocusPasswordInput";export{M as A,c as P};
---
+++ assets/RepoCreatePage-q9w_7T6o.js
@@ -0,0 +1,136 @@
+import{r as i,u as g,j as e,a as y}from"./DynamicThemeProvider-MqskYVZg.js";import{M as V,b as H,c as q,d as A,e as K,f as G,g as I,h as C,a as k,i as v,T as j,u as O}from"./mainWebAuthenticated-zAhhv6Xh.js";import{D as U}from"./DashboardLayout-Dya9WcNn.js";import{N as W,u as Y}from"./DashboardNavbar-D-wPUt3i.js";import{u as _}from"./useDocumentTitle-D3L4F_h2.js";import{u as J,c as o,a as b}from"./emotion-css.esm-D9bkNW-b.js";import{B as p,b as Q,s as w}from"./Button-D3442RQe.js";import{R as X}from"./RepoConfigInfo-CQSjbBW8.js";import{P as Z}from"./PasswordInput-NxJ31T9d.js";import{D as ee}from"./DirPicker-B0eNGv6i.js";import"./NavbarSticky-ClW7pS7A.js";import"./index-D1Fh1Ers.js";import"./GitRevision-BXe6WO9K.js";import"./Navbar-D-Wj6WD0.js";import"./range-fR_g9B17.js";const N=i.memo(({created:a})=>{const r=g(),s=J(),[n,t]=i.useState(!1),l=i.useCallback(()=>{a!==void 0&&s(`/repos/${a.repoId}`)},[s,a]);return e.jsxs("div",{children:[e.jsx("h1",{className:o`
+            font-size: 28px;
+            font-weight: normal;
+            margin: 0 0 20px;
+          `,children:"Your Safe Box has been created."}),e.jsx("p",{className:o`
+            margin: 0 0 20px;
+          `,children:"Before you start using your Safe Box please safely store the configuration."}),e.jsx("div",{className:o`
+            border-bottom: 1px solid ${r.colors.border};
+            margin-bottom: 25px;
+          `}),e.jsx("div",{className:o`
+            margin-bottom: 25px;
+          `,onMouseDown:()=>t(!0),children:e.jsx(X,{config:a.config})}),e.jsx("div",{className:o`
+            border-bottom: 1px solid ${r.colors.border};
+            margin-bottom: 25px;
+          `}),e.jsx(p,{type:"button",variant:n?"primary":"disabled",disabled:!n,onClick:l,children:"Continue"})]})});N.displayName="RepoCreateCreatedComponent";const S=i.memo(({breadcrumbs:a})=>{const r=i.useMemo(()=>a.map((s,n)=>({id:s.id,name:s.name,isClickable:!1,hasCaret:!1,isLast:n===a.length-1})),[a]);return e.jsx(W,{breadcrumbs:r})});S.displayName="RemoteFilesBreadcrumbs";const D=i.memo(({dirPickerId:a,onClick:r,canSelect:s,select:n,cancel:t,createDirEnabled:l,createDir:d})=>{const m=y(),x=g();return e.jsxs(e.Fragment,{children:[e.jsx(H,{children:e.jsx(q,{children:"Select a folder"})}),e.jsx(A,{className:o`
+            padding-bottom: 0;
+            overflow: hidden;
+          `,children:e.jsx("div",{className:b(o`
+                border: 1px solid ${x.colors.borderDarker};
+                border-radius: 3px;
+                overflow-y: scroll;
+                overflow-x: hidden;
+              `,m?o`
+                    flex-grow: 1;
+                  `:o`
+                    height: 300px;
+                  `),children:e.jsx(ee,{pickerId:a,onClick:r})})}),e.jsxs(K,{children:[e.jsx(G,{children:e.jsx(p,{type:"button",disabled:!l,onClick:d,children:"Create folder"})}),e.jsxs(I,{children:[e.jsx(C,{type:"button",onClick:t,children:"Cancel"}),e.jsx(C,{type:"button",variant:s?"primary":"disabled",disabled:!s,onClick:n,children:"Select"})]})]})]})});D.displayName="RemoteFilesDirPickerModalContent";const R=i.memo(({dirPickerId:a,onClick:r,canSelect:s,select:n,cancel:t,createDirEnabled:l,createDir:d})=>e.jsx(V,{show:a!==void 0,onHide:t,children:a!==void 0?e.jsx(D,{dirPickerId:a,onClick:r,canSelect:s,select:n,cancel:t,createDirEnabled:l,createDir:d}):e.jsx(e.Fragment,{})}));R.displayName="RemoteFilesDirPickerModal";const u=w.label`
+  display: inline-block;
+  display: inline-block;
+  font-weight: 600;
+  font-size: 13px;
+  margin-bottom: 2px;
+`,h=w.div`
+  margin-bottom: 10px;
+`,oe=`Format:
+
+[name]
+type=crypt
+remote=rcloneremote:/path
+password=obscured password
+password2=obscured salt`,F=i.memo(({createId:a,form:r})=>{const s=y(),n=g(),t=k(),[l,d]=i.useState(!1),[m,x]=i.useState(""),M=i.useCallback((c,T,$)=>t.repoCreateLocationDirPickerClick(a,T,$),[t,a]),B=i.useCallback(()=>{t.repoCreateLocationDirPickerSelect(a)},[t,a]),L=i.useCallback(()=>{t.repoCreateLocationDirPickerCancel(a)},[t,a]),z=i.useCallback(()=>{t.repoCreateLocationDirPickerCreateDir(a)},[t,a]),E=i.useCallback(c=>{c.preventDefault(),t.repoCreateCreateRepo(a)},[t,a]),f=r.canCreate&&r.createRepoStatus.type!=="Loading";return r.createLoadStatus.type==="Initial"||r.createLoadStatus.type==="Loading"?e.jsx(v,{}):e.jsxs("div",{className:s?o`
+              padding: 0 15px;
+            `:void 0,children:[e.jsx("h1",{className:o`
+          font-size: 32px;
+          font-weight: normal;
+          margin: 0 0 20px;
+        `,children:"Create a new Safe Box"}),e.jsx("form",{onSubmit:E,children:e.jsxs("div",{className:b(o`
+              display: flex;
+            `,s?o`
+                  flex-direction: column;
+                `:o`
+                  flex-direction: row;
+                `),children:[e.jsxs("div",{className:b(o`
+                flex-shrink: 0;
+              `,s?o`
+                    width: 100%;
+                  `:o`
+                    width: 370px;
+                    margin-right: 70px;
+                  `),children:[r.createRepoStatus.type==="Error"?e.jsx("div",{className:o`
+                  background-color: #fbedeb;
+                  padding: 6px 15px;
+                  border-radius: 3px;
+                  margin-bottom: 15px;
+                `,children:r.createRepoStatus.error}):null,e.jsxs(h,{children:[e.jsx(u,{children:"Location"}),e.jsx("button",{type:"button",className:o`
+                  ${Q}
+                  text-align: left;
+                  border: 1px solid ${n.colors.borderDark};
+                  border-radius: 3px;
+                  display: inline-block;
+                  padding: 9px 10px 8px;
+                  font-size: 14px;
+                  width: 100%;
+                  cursor: pointer;
+
+                  &:focus {
+                    border-color: ${n.colors.primary};
+                  }
+                `,onClick:()=>{t.repoCreateLocationDirPickerShow(a)},children:r.locationBreadcrumbs.length>0?e.jsx(S,{breadcrumbs:r.locationBreadcrumbs}):e.jsx("span",{children:"Select a folder"})})]}),e.jsxs(h,{children:[e.jsx(u,{htmlFor:"password",className:o`
+                  margin-bottom: 5px;
+                  display: inline-block;
+                `,children:"Safe Key"}),e.jsx(Z,{value:r.password,placeholder:"Must be at least 8 characters long",onChange:c=>t.repoCreateSetPassword(a,c),inputClassName:o`
+                  display: block;
+                  width: 100%;
+                  font-size: 14px;
+                `,inputId:"password"})]}),l?e.jsxs(h,{children:[e.jsx(u,{htmlFor:"salt",className:o`
+                    margin-bottom: 5px;
+                    display: inline-block;
+                  `,children:"Salt"}),e.jsx(j,{as:"textarea",id:"salt",value:r.salt??"",className:o`
+                    display: block;
+                    width: 100%;
+                    height: 180px;
+                    margin-bottom: 10px;
+                  `,onChange:c=>t.repoCreateSetSalt(a,c.currentTarget.value.length>0?c.currentTarget.value.trim():void 0)}),e.jsxs("div",{className:o`
+                    background-color: #d1ecf1;
+                    border-radius: 3px;
+                    padding: 12px 15px;
+                    font-size: 13px;
+                    margin-bottom: 15px;
+                  `,children:[e.jsx("p",{className:o`
+                      margin: 0 0 15px;
+                    `,children:"Salt is used in the key derivation process to create a unique encryption key and helps to protect against potential attacks. It will be stored on the Koofr servers in a secure manner."}),e.jsx("p",{className:o`
+                      margin: 0 0 15px;
+                    `,children:"A random Salt has been generated for you. If you prefer, you can leave the Salt field empty, and the default salt will be used (same as in rclone). However, it is recommended to use a unique salt for enhanced security. Using a unique salt helps to increase the complexity of the encryption process, making it more difficult for potential attackers to access the encrypted data."}),e.jsx("p",{className:o`
+                      margin: 0;
+                    `,children:"If you wish to transfer the encrypted files to another service, it is necessary to also export the salt, otherwise you won't be able to decrypt your files."})]})]}):null,e.jsx("div",{className:o`
+                margin: 15px 0 25px;
+              `,children:e.jsx(p,{type:"submit",variant:f?"primary":"disabled",disabled:!f,children:"Create"})}),l?null:e.jsx("div",{children:e.jsx(p,{type:"button",variant:"primary-inline",className:o`
+                    padding: 0;
+                  `,onClick:()=>d(!0),children:"Show advanced settings"})})]}),l?e.jsxs("div",{className:s?o`
+                      width: 100%;
+                    `:o`
+                      width: 400px;
+                    `,children:[e.jsx("h3",{className:o`
+                  font-size: 28px;
+                  font-weight: normal;
+                  margin: 23px 0 20px;
+                `,children:"From rclone config"}),e.jsx("pre",{className:o`
+                  background-color: #f8f8f8;
+                  color: ${n.colors.textLight};
+                  border-radius: 3px;
+                  font-size: 13px;
+                  padding: 6px 15px;
+                `,children:e.jsx("code",{children:oe})}),r.fillFromRcloneConfigError!==void 0?e.jsx("div",{className:o`
+                    background-color: #fbedeb;
+                    padding: 6px 15px;
+                    border-radius: 3px;
+                    margin-bottom: 15px;
+                  `,children:r.fillFromRcloneConfigError}):null,e.jsx("div",{className:o`
+                  margin-bottom: 15px;
+                `,children:e.jsx(j,{as:"textarea",value:m,className:o`
+                    display: block;
+                    width: 100%;
+                    height: 250px;
+                    font-size: 13px;
+                  `,onChange:c=>x(c.currentTarget.value)})}),e.jsx(p,{type:"button",variant:"primary",onClick:()=>{t.repoCreateFillFromRcloneConfig(a,m)},children:"Fill"})]}):null]})}),e.jsx(R,{dirPickerId:r.locationDirPickerId,onClick:M,canSelect:r.locationDirPickerCanSelect,select:B,cancel:L,createDirEnabled:r.locationDirPickerCreateDirEnabled,createDir:z})]})});F.displayName="RepoCreateFormComponent";const P=i.memo(()=>{const a=k(),r=i.useMemo(()=>a.repoCreateCreate(),[a]),[s]=O((t,l)=>t.repoCreateInfoSubscribe(r,l),t=>t.repoCreateInfoData,[]);i.useEffect(()=>()=>{a.repoCreateDestroy(r)},[a,r]);const n=Y("Create a new Safe Box");return _("Create a new Safe Box"),e.jsx(U,{navbarHeader:n,children:s?.type==="Form"?e.jsx(F,{createId:r,form:s}):s?.type==="Created"?e.jsx(N,{created:s}):e.jsx(v,{})})});P.displayName="RepoCreate";const ae=i.memo(()=>e.jsx(P,{}));ae.displayName="RepoCreatePage";export{ae as RepoCreatePage};
