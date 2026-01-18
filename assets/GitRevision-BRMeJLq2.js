77d778c3c04fff3c8c93d4eec02b026a44c020cb
bancek <367440+bancek@users.noreply.github.com>
2026-01-18 08:28:34 +0000

    Update assets

---
+++ assets/GitRevision-BRMeJLq2.js
@@ -0,0 +1,9 @@
+import{r as t,j as r}from"./DynamicThemeProvider-MqskYVZg.js";import{c as i}from"./emotion-css.esm-D9bkNW-b.js";import{d as o}from"./Button-D3442RQe.js";const a=t.memo(({className:s})=>{const e="v0.1.22";return r.jsxs("div",{className:s,children:["Version:"," ",r.jsx("a",{href:`https://github.com/koofr/vault/releases/tag/${e}`,target:"_blank",rel:"noreferrer",className:i`
+          ${o} {
+            color: inherit;
+          }
+        `,children:e})]})});a.displayName="GitRelease";const l=t.memo(({className:s})=>{const e="bff1b42";return r.jsxs("div",{className:s,children:["Git revision:"," ",r.jsx("a",{href:`https://github.com/koofr/vault/commit/${e}`,target:"_blank",rel:"noreferrer",className:i`
+          ${o} {
+            color: inherit;
+          }
+        `,children:e})]})});l.displayName="GitRevision";export{a as G,l as a};
