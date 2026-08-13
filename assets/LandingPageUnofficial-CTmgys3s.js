fcaaf66694e9366722f32996e0eb7b19ada485ed
jkmcnk <4378371+jkmcnk@users.noreply.github.com>
2026-08-13 15:55:18 +0000

    Update assets

---
+++ assets/LandingPageUnofficial-CTmgys3s.js
@@ -0,0 +1,101 @@
+import{r as e}from"./chunk-DECur_0Z.js";import"./emotion-react.browser.esm-B6XXdZtW.js";import{n as t,t as n}from"./emotion-css.esm-Qs1dyEi9.js";import{n as r,t as i}from"./jsx-runtime-BA9giqqw.js";import"./injectIntl-DcBbzoym.js";import{u as a}from"./chunk-LFPYN7LY-4vWiejj4.js";import{a as o,o as s}from"./Button-CQrJ5P7o.js";import{t as c}from"./Button-DtOAjgWk.js";import"./useClickOutside-DDfmGtM1.js";import"./IntlLocalesContext-CX3kKNf2.js";import{t as l}from"./useDocumentTitle--9KSRPbr.js";import"./useMenuUpdate-uUH7xBSR.js";import{n as u,r as d,t as f}from"./GitRevision-C4_Z_NFJ.js";import{t as p}from"./logo-C4Nvk3N4.js";var m=e(r(),1),h=i(),g=o(`#1683fb`,`#1683fb`,`#ffffff`,`#0576f1`,`#0576f1`,`#ffffff`),_={smMaxWidth:767,mdMinWidth:768,mdMaxWidth:1048,lgMinWidth:1049,lgMaxWidth:1365,xlMinWidth:1366},v={sm:`@media (max-width: ${_.smMaxWidth}px)`,smmd:`@media (max-width: ${_.mdMaxWidth}px)`,md:`@media (min-width: ${_.mdMinWidth}px) and (max-width: ${_.mdMaxWidth}px)`,mdlg:`@media (min-width: ${_.mdMinWidth}px) and (max-width: ${_.lgMaxWidth}px)`,lg:`@media (min-width: ${_.lgMinWidth}px) and (max-width: ${_.lgMaxWidth}px)`,lgxl:`@media (min-width: ${_.lgMinWidth}px)`,xl:`@media (min-width: ${_.xlMinWidth}px)`},y=(0,m.memo)(()=>(l(),(0,h.jsxs)(`div`,{className:n`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        min-height: 100vh;
+      `,children:[(0,h.jsx)(`div`,{className:n`
+          width: 100%;
+          padding-top: 20px;
+          padding-bottom: 20px;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: center;
+          margin-bottom: 30px;
+          flex-shrink: 0;
+
+          ${v.sm} {
+            padding-left: 15px;
+            padding-right: 15px;
+            padding-top: 7px;
+            padding-bottom: 15px;
+          }
+
+          ${v.md} {
+            padding-top: 20px;
+          }
+
+          ${v.mdlg} {
+            padding-left: 28px;
+            padding-right: 28px;
+          }
+
+          ${v.xl} {
+            width: 1280px;
+          }
+        `,children:(0,h.jsx)(p,{})}),(0,h.jsx)(`div`,{className:n`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          flex-grow: 1;
+
+          ${v.sm} {
+            padding-left: 15px;
+            padding-right: 15px;
+          }
+
+          ${v.smmd} {
+            margin: 0 0 60px;
+          }
+
+          ${v.lgxl} {
+            margin: 0 0 111px;
+          }
+        `,children:(0,h.jsxs)(`div`,{className:n`
+            display: flex;
+            flex-direction: column;
+            max-width: 500px;
+          `,children:[(0,h.jsx)(`h1`,{className:n`
+              font-size: 64px;
+              font-weight: 700;
+              color: #011722;
+              flex-grow: 1;
+              margin: 0 0 22px;
+              text-align: center;
+            `,children:`Koofr Vault`}),(0,h.jsx)(`p`,{className:n`
+              font-size: 18px;
+              color: #011722;
+              margin: 0 0 38px;
+              text-align: center;
+            `,children:(0,h.jsx)(a,{id:`web.landing_page_unofficial.warning`,description:`Warning text on the unofficial landing page informing users it's not an official site.`,defaultMessage:`This is an unofficial Koofr Vault page. Use at your own risk.`})}),(0,h.jsx)(c,{href:`/login`,className:t(g,n`
+                width: 100%;
+                font-size: 18px;
+                padding: 9px 21px;
+                font-weight: 700;
+              `),children:(0,h.jsx)(a,{id:`web.landing_page.get_started.button`,description:`Primary call-to-action button on the landing page that starts login.`,defaultMessage:`Get started`})})]})}),(0,h.jsx)(`div`,{className:n`
+          width: 100%;
+          border-bottom: 1px solid #d4d6d7;
+        `}),(0,h.jsxs)(`div`,{className:n`
+          ${v.smmd} {
+            padding: 30px 28px;
+          }
+
+          ${v.lgxl} {
+            padding: 40px 28px;
+          }
+        `,children:[(0,h.jsx)(`p`,{className:n`
+            font-size: 14px;
+            color: #011722;
+            text-align: center;
+            margin: 0 0 15px;
+          `,children:(0,h.jsx)(a,{id:`web.landing_page.copyright.text`,description:`Footer copyright line on the landing page with company link and current year.`,defaultMessage:`© {year}. <a>Koofr d.o.o.</a> all rights reserved.`,values:{year:new Date().getFullYear(),a:e=>(0,h.jsx)(`a`,{href:`https://koofr.eu`,target:`_blank`,rel:`noopener`,className:n`
+                    font-weight: 600;
+                    ${s} {
+                      color: #011722;
+                      text-decoration: none;
+                    }
+                  `,children:e})}})}),(0,h.jsxs)(`div`,{className:n`
+            font-size: 13px;
+            color: #011722;
+            text-align: center;
+          `,children:[(0,h.jsx)(u,{}),(0,h.jsx)(f,{})]}),(0,h.jsx)(d,{})]})]})));y.displayName=`LandingPageUnofficial`;export{y as LandingPageUnofficial};
---
+++ assets/mainWebNotSupported-wtmKWOJK.js
@@ -0,0 +1,18 @@
+import{r as e}from"./chunk-DECur_0Z.js";import"./emotion-react.browser.esm-B6XXdZtW.js";import{t}from"./emotion-css.esm-Qs1dyEi9.js";import{n,t as r}from"./jsx-runtime-BA9giqqw.js";import"./injectIntl-DcBbzoym.js";import"./DynamicThemeProvider-DMWnpZXO.js";import{t as i}from"./useIntl-uVUnHosr.js";import{i as a,u as o}from"./chunk-LFPYN7LY-4vWiejj4.js";import"./config-D41TpNeF.js";import"./react-dom-D7VaEYON.js";import{t as s}from"./dom-export-DRq9bQqW.js";import"./useIsMobile-Dv4RoKy5.js";import"./DocumentScroll-BSm_K0jh.js";import"./NavbarSticky-OZG2b-qJ.js";import"./IntlLocalesContext-CX3kKNf2.js";import"./LandingPage-DlDNj4ji.js";import{mainUnauthenticated as c}from"./mainWebUnauthenticated-CEptlCJo.js";import{t as l}from"./error-icon@2x-MthBeAQr.js";import{t as u}from"./Navbar-CTm4q7Rg.js";var d=e(n(),1),f=r(),p=(0,d.memo)(()=>{let e=i().formatMessage({id:`web.not_supported_page.title`,description:`Document title for the browser not supported page.`,defaultMessage:`Not supported`});return(0,f.jsxs)(f.Fragment,{children:[(0,f.jsx)(u,{header:(0,f.jsx)(`span`,{className:t`
+              font-weight: 600;
+            `,children:e})}),(0,f.jsxs)(`div`,{className:t`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        `,children:[(0,f.jsx)(`img`,{src:l,alt:e,className:t`
+            display: block;
+            width: 252px;
+            height: 186px;
+            margin: 0 0 30px;
+          `}),(0,f.jsx)(`h2`,{className:t`
+            font-size: 32px;
+            font-weight: normal;
+            margin: 0 0 30px;
+            max-width: 500px;
+            text-align: center;
+          `,children:(0,f.jsx)(o,{id:`web.not_supported_page.description`,description:`Message shown when the current browser is not supported.`,defaultMessage:`Your browser is not supported. Please open this page in a modern browser on a computer.`})})]})]})});p.displayName=`NotSupportedPage`;var m=async()=>{await c((0,f.jsx)(s,{router:a([{path:`*`,element:(0,f.jsx)(p,{})}])}))};export{m as mainNotSupported};
